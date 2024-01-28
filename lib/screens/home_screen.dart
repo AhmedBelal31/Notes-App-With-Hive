afda50b2a2a6bfdc7687741a68f7f283a3ad1514
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-01-28 21:51:20 +0200

    Create ModalBottomSheet To Add Note

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,17 +2,69 @@ import 'package:flutter/material.dart';
 import 'package:notes_app_with_hive/widgets/custom_notes_list_view.dart';
 import '../widgets/custom_app_bar.dart';
 
-
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-final List<Color> noteColor =const [Colors.red , Colors.blue , Colors.orange , Colors.green];
+
+  final List<Color> noteColor = const [
+    Colors.red,
+    Colors.blue,
+    Colors.orange,
+    Colors.green
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       floatingActionButton: FloatingActionButton(
-        onPressed:(){},
-        child:const Icon(Icons.add),
+        onPressed: () {
+          showModalBottomSheet(
+              isScrollControlled: true, // Set to true to enable full-height sheet
+              showDragHandle: true,
+              context: context,
+              builder: (context) => Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  child: SizedBox(
+                    height: MediaQuery.of(context).size.height/2,
+                    child: Column(
+                          children: [
+                            TextField(
+                              decoration: InputDecoration(
+                                hintText: '  Title',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(16.0),
+                                )
+
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 20),
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                    contentPadding:const EdgeInsets.symmetric(vertical: 60.0),
+                                    hintText: '  Content',
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(16.0),
+                                    )
+
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: double.infinity,
+                              child: FilledButton(
+                                onPressed: () {},
+                                child: Text('Add'),
+                              ),
+                            ),
+                          ],
+                        ),
+                  ),
+                ),
+              ));
+        },
+        child: const Icon(Icons.add),
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 12.0),
@@ -23,7 +75,6 @@ final List<Color> noteColor =const [Colors.red , Colors.blue , Colors.orange , C
               const SizedBox(height: 50),
               const CustomAppBar(),
               CustomNotesListView(noteColor: noteColor),
-
             ],
           ),
         ),
