845168b52f9b705c2d143d40c78baf4d2f1631f3
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-01-28 21:29:46 +0200

    Create Custom Notes ListView

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,29 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app_with_hive/widgets/custom_notes_list_view.dart';
 
 import '../widgets/custom_app_bar.dart';
 import '../widgets/custom_note_item.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
+final List<Color> noteColor =const [Colors.red , Colors.blue , Colors.orange , Colors.green];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 12.0),
         child: SingleChildScrollView(
-          physics:const BouncingScrollPhysics(),
+          physics: const BouncingScrollPhysics(),
           child: Column(
             children: [
               const SizedBox(height: 50),
               const CustomAppBar(),
-              ListView.separated(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemBuilder: (context, index) =>const CustomNoteItem(),
-                separatorBuilder: (context, index) =>const SizedBox(height: 10),
-                itemCount: 8,
-              ),
+              CustomNotesListView(noteColor: noteColor),
+
             ],
           ),
         ),
