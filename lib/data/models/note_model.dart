51192561e4fd724d1951f8510ead10c9e5f5a9f1
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-01-29 19:34:15 +0200

    Create Note Model

---
+++ lib/data/models/note_model.dart
@@ -0,0 +1,16 @@
+class NoteModel {
+  String title;
+
+  String subTitle;
+
+  String data;
+
+  int color;
+
+  NoteModel({
+    required this.title,
+    required this.subTitle,
+    required this.data,
+    required this.color,
+  });
+}
